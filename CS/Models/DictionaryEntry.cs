c876859f5a264011852407ccd44fa9e6199c1854
John Limos <103324036+jvlimos@users.noreply.github.com>
2025-11-18 15:05:41 +0800

    Add initial draft

---
+++ CS/Models/DictionaryEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPxGridViewAIIntegration.Models
+{
+    public class DictionaryEntry
+    {
+        public DictionaryEntry(int id, string name, string description)
+        {
+            ID = id;
+            Name = name;
+            Description = description;
+        }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
